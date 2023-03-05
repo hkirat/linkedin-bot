9c9097ccb23fc2be96c0d8411a4586c868380aa4
lakk14 <laukikkakade8@gmail.com>
2023-03-05 13:35:33 +0530

    fix(SslError): Fix ssl handshake and code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { Builder, By, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
 const BasePage = require("./WebPage")
 
 async function sleep(timeInS) {
@@ -9,18 +7,18 @@ async function sleep(timeInS) {
 async function startBot() {
 	const page = new BasePage();
 	let site = "https://www.linkedin.com";
-        await page.visit(site);
 
-        await sleep(5)
-
+	await page.visit(site);
+	await sleep(5)
 	await page.signin();
-        await sleep(20) 
-        
+	await sleep(20)
+
 	site = "https://www.linkedin.com/mynetwork/invitation-manager";
-        await sleep(5)
+
+	await sleep(5)
 	await page.visit(site);
 
-	for (let i = 3; i< 10000; i++) {
+	for (let i = 3; i < 10000; i++) {
 		if (i % 10 === 9) {
 			page.visit(site);
 			await sleep(8)
@@ -28,17 +26,16 @@ async function startBot() {
 			await sleep(2)
 		}
 		try {
-            		await page.pressAcceptButton();
-            		await new Promise((resolve) => setTimeout(resolve, 1000))
-	    		page.scrollToBottom();	
-	       		await sleep(3)
-		} catch(e) {
-		 	console.error(e)
+			await page.pressAcceptButton();
+			await new Promise((resolve) => setTimeout(resolve, 1000))
+			page.scrollToBottom();
+			await sleep(3)
+		} catch (e) {
+			console.error(e)
 		}
 	}
-        await new Promise((resolve) => setTimeout(resolve, 5000))
+	await new Promise((resolve) => setTimeout(resolve, 5000))
 }
-
 
 (async () => {
 	await startBot()
